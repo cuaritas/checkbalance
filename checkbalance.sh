18c91a7d56966ca8fa168b55fe090fcaa1ff527e
Cuaritas <88283485+cuaritas@users.noreply.github.com>
2021-09-07 14:37:23 -0500

    Update checkbalance.sh

--- checkbalance.sh
+++ checkbalance.sh
@@ -6,10 +6,15 @@
 
 # Use as a systemd service for more usability
 
-BALANCE_FILEPATH="$HOME/.balance"
-LNCLI="/usr/bin/lncli"
-WEBHOOK="https://xxxxxxxxxxxxxxxxxx.m.pipedream.net"
-balance=`$LNCLI channelbalance | jq .balance`
+BALANCE_FILEPATH="/home/umbrel/.balance"
+MACAROON_HEADER="Grpc-Metadata-macaroon: $(xxd -ps -u -c 1000 /home/umbrel/.lnd/admin.macaroon)"
+WEBHOOK="https://xxxxxxxxxxxxxxx.m.pipedream.net"
+
+while sleep 3600; do
+
+commit=`curl -s -X GET --cacert /home/umbrel/.lnd/tls.cert --header "$MACAROON_HEADER" https://umbrel.local:8080/v1/channels | jq -r '.channels[].commit_fee' | awk '{sum+=$1} END {print sum}'`
+local=`curl -s -X GET --cacert /home/umbrel/.lnd/tls.cert --header "$MACAROON_HEADER" https://umbrel.local:8080/v1/channels | jq -r '.channels[].local_balance' | awk '{sum+=$1} END {print sum}'`
+balance=$((local+commit))
 
 if ! [[ -f $BALANCE_FILEPATH ]]; then
   echo $balance > $BALANCE_FILEPATH
@@ -17,5 +22,11 @@ fi
 
 cat $BALANCE_FILEPATH | grep -q $balance
 if [[ $? -ne 0 ]]; then # send notification using Pipedream
-  curl -s -d "{balance:$balance}" -H "Content-Type: application/json" $WEBHOOK
+  echo $balance > $BALANCE_FILEPATH
+  curl -s -F "balance=$balance" $WEBHOOK
+  echo
+else
+  echo "Node balance $balance has not changed"
 fi
+
+done&
